df73210f6cb94d533a952f3f7d2e9098732b804b
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-09 15:54:05 +0100

    Asignando los atributos

--- web-components/card/cardDos.js
+++ web-components/card/cardDos.js
@@ -1,4 +1,8 @@
 class Card2 extends HTMLElement {
+  cardBordeRadios = "5rem";
+  cardPadding = "1rem";
+  cardHeight = "27rem";
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
