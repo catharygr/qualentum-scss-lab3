e290589530f4625624f973081d89805b2999d8a8
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-09 16:45:54 +0100

    Actualizando con render  el contenido del componente en el DOM.

--- web-components/card/cardDos.js
+++ web-components/card/cardDos.js
@@ -12,7 +12,7 @@ class Card2 extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name);
+    console.log(this.attributeChangedCallback(name, oldValue, newValue)));
     this.attributesMap[name] = newValue;
     this.render();
   }
